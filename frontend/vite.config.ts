9813880de505709d7834694e6f62f019217de06b
Ewan Senergous <ewan16270409@outlook.fr>
2025-06-30 10:54:47 +0200

    :bug: fix One Drive Error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     watch: {
       usePolling: true,
